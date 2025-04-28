3b92e4a36a1a3671cae4a4d08a7be3efa4c8e899
oacommit <oainit25@gmail.com>
2025-04-28 21:00:19 +0200

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -1,17 +1,20 @@
+
 from flask import Flask, request, redirect, render_template_string
 import redis
 import os
 
 app = Flask(__name__)
 redis_host = os.getenv('REDIS_HOST', 'redis')
-redis_conn = redis.StrictRedis(host=redis_host, port=6379, db=0, decode_responses=True)
+redis_conn = redis.StrictRedis(host=redis_host, port=6379, db=0)
 
 TEMPLATE = '''
 <h1>Vote for your favorite!</h1>
 <form method="POST">
-    <button type="submit" name="vote" value="Cats">Cats</button> ({{ cats_count }} votes)<br><br>
-    <button type="submit" name="vote" value="Dogs">Dogs</button> ({{ dogs_count }} votes)
+    <button type="submit" name="vote" value="Cats">Cats</button>
+    <button type="submit" name="vote" value="Dogs">Dogs</button>
 </form>
+<p>Cats: {{ cats_count }}</p>
+<p>Dogs: {{ dogs_count }}</p>
 '''
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,8 +26,11 @@ def vote():
 
     # Fetch votes from Redis
     votes = redis_conn.lrange('votes', 0, -1)  # Get all votes
-    cats_count = votes.count('Cats')
-    dogs_count = votes.count('Dogs')
+
+    # lrange returns a list of bytes, so decode first
+    decoded_votes = [v.decode('utf-8') for v in votes]
+    cats_count = decoded_votes.count('Cats')
+    dogs_count = decoded_votes.count('Dogs')
 
     return render_template_string(TEMPLATE, cats_count=cats_count, dogs_count=dogs_count)
 
